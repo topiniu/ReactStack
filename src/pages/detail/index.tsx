6f20774e1f1752cceb44309e92481caa28ce6e54
topiniu <topiniu@outlook.com>
2019-04-08 11:09:39 +0800

    feat(detail.js):

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -12,7 +12,7 @@ class Detail extends React.Component<any,any>{
   render(){
     return (
       <div>
-        <p className="text">page2 hello</p>
+        <p className="text">page2 hello I am page 2(detail)</p>
         page2
         <Link to="/">Go to P1</Link>
         {/* <Link to="/page2/self">to Self</Link> */}
